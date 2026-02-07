0e6f3a1b99e78dc877235df4196aa2cd15744525
jaggiiii <duppalapudijagadeesh5@gmail.com>
2026-02-08 00:43:09 +0530

    added testing for backend 1.0

---
+++ backend/test/basic.test.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const vitest_1 = require("vitest");
+(0, vitest_1.test)("basic test passes", () => {
+    (0, vitest_1.expect)(1 + 1).toBe(2);
+});
